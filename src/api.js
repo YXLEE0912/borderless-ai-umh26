5a705cb07e29e73902dbb890eefcf3874eb67bc8
Kuan0315 <24004581@siswa.um.edu.my>
2026-04-25 23:12:54 +0800

    delete

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export async function searchHS(keyword) {
-  const response = await fetch(
-    `${import.meta.env.VITE_API_URL}/hs/search?keyword=${keyword}`
-  );
-  if (!response.ok) {
-    throw new Error("API request failed");
-  }
-  return await response.json();
-}
